68f06dff33e5d476b2c9ed95431e8a789ba05f9e
yavasogluM <40884148+yavasogluM@users.noreply.github.com>
2022-10-03 22:50:45 +0300

    added serialize/deserialize functions

---
+++ PhoneBook.Extensions/PhoneBook.Extensions/SerializationExtension.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PhoneBook.Extensions
+{
+    public class SerializationExtension
+    {
+        public static T DeSerialize<T>(string objectStr)
+        {
+            if(string.IsNullOrEmpty(objectStr)) return Activator.CreateInstance<T>();
+            return JsonConvert.DeserializeObject<T>(objectStr);
+        }
+
+        public static string Serialize<T>(T obj)
+        {
+            if(obj == null) return string.Empty;
+            return JsonConvert.SerializeObject(obj);
+        }
+    }
+}
