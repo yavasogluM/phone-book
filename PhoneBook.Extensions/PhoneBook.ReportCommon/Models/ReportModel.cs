f9efa9a28626a8f3a88d0d6d8870910f74a3f178
yavasogluM <40884148+yavasogluM@users.noreply.github.com>
2022-10-07 00:22:08 +0300

    added report model and report service

---
+++ PhoneBook.Extensions/PhoneBook.ReportCommon/Models/ReportModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PhoneBook.ReportCommon.Models
+{
+    public class ReportModel
+    {
+        public int UUID { get; set; }
+        public DateTime TalepTarihi { get; set; }
+        public ReportDetail RaporDetay { get; set; }
+        public ReportType Durum { get; set; }
+    }
+
+    public class ReportDetail
+    {
+        public string Konum { get; set; }
+        public int KisiSayisi { get; set; }
+        public int TelefonNumarasiSayisi { get; set; }
+    }
+
+    public enum ReportType
+    {
+        Preparing = 0,
+        Completed = 1
+    }
+}
---
+++ PhoneBook.Extensions/PhoneBook.ReportCommon/Services/ReportService.cs
@@ -0,0 +1,19 @@
+﻿using PhoneBook.ReportCommon.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PhoneBook.ReportCommon.Services
+{
+    public interface IReportService
+    {
+        List<ReportModel> GetAll();
+    }
+    public class ReportService : IReportService
+    {
+        public List<ReportModel> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
