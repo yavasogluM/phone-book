d6ba06c2fccd3c335d3223dbbc14cb70aaa45b91
yavasogluM <40884148+yavasogluM@users.noreply.github.com>
2022-10-07 01:09:05 +0300

    added mongodb base

---
+++ PhoneBook.Extensions/PhoneBook.Extensions/MongoDB/BaseCollection.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace PhoneBook.Extensions.MongoDB
+{
+    public class BaseCollection
+    {
+        [BsonElement("_id")]
+        public ObjectId Id { get; set; }
+
+        public string RowId => Id.ToString();
+    }
+}
---
+++ PhoneBook.Extensions/PhoneBook.Extensions/MongoDB/MongoDBConnectionSetting.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PhoneBook.Extensions.MongoDB
+{
+    public class MongoDBConnectionSetting
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string DBName { get; set; }
+        public string CollectionName { get; set; }
+    }
+}
